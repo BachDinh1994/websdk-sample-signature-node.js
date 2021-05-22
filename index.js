c4d7d0c4ffce2ebb8c147e5017cd775cf5530f77
Bach <bachdinh@usc.edu>
2021-05-23 01:38:13 +0700

    test json

--- index.js
+++ index.js
@@ -31,10 +31,10 @@ app.post('/', (req, res) =>
     let userId = "10223533493112883";
     let accessToken = "EAAHRJy2tE4EBAAGZBvxzRaN9lC8JeeK0gv3IPp5VmvYWqBd2mDl7mqx7jsSpbiHEZAiREt6CN8fRoIe0IcVuMcdB2i1U7SMbcEcCZCi2rgUlAOxv5ZA6Q86AqDJNmnqSfkHCblJhPN4cgYPTPoDgAQLCzAiP438KZA3yQj9IFcMka1ZC7po87mlcvTvKDsss7pY60aVL7HuwZDZD";
     let request = fbGraph.replace("my_id", userId).replace("my_token", accessToken);
-    /*const timestamp = new Date().getTime() - 30000
+    const timestamp = new Date().getTime() - 30000
     const msg = Buffer.from(process.env.ZOOM_JWT_API_KEY + req.body.meetingNumber + timestamp + req.body.role).toString('base64')
     const hash = crypto.createHmac('sha256', process.env.ZOOM_JWT_API_SECRET).update(msg).digest('base64')
-    const signature = Buffer.from(`${process.env.ZOOM_JWT_API_KEY}.${req.body.meetingNumber}.${timestamp}.${req.body.role}.${hash}`).toString('base64')*/
+    const signature = Buffer.from(`${process.env.ZOOM_JWT_API_KEY}.${req.body.meetingNumber}.${timestamp}.${req.body.role}.${hash}`).toString('base64')
 
     let jsonValue = getJSON(request,
         function (err, data) {
@@ -44,7 +44,9 @@ app.post('/', (req, res) =>
                 return data;
             }
         });
-    res.json(accessToken)
+    res.json({
+        signature: "hello world"
+    })
 })
 
 app.listen(port, () => console.log(`Zoom Web Client SDK Sample Signature Node.js on port ${port}!`))
